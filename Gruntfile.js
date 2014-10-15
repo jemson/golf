67b0350ee7c0f6d4cab864dedcb8cdf5391af752
Kristoffer Robin Canlas <robin.canlas@geekgig.com>
2014-10-15 11:51:09 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,108 @@
+'use strict';
+
+module.exports = function(grunt){
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		compass: {
+			compile: {
+				options: {
+					config: 'config.rb',
+					force: true,
+				},
+			}
+		},
+		jshint: {
+		    options: {
+		        jshintrc: '.jshintrc',
+		        reporter: require('jshint-stylish'),
+		        ignores: ['public/scripts/libs/*']
+		    },
+		    all: [
+		        'Gruntfile.js',
+		        'public/scripts/{,*/}*.js',
+		    ]
+		},
+		mocha: {
+            urls: ['http://localhost:8001/test.html'],
+			options: {
+				run: true,
+				log: true,
+			}
+		},
+		watch: {
+			css: {
+				files: ['public/styles/**/*.scss'],
+				tasks: ['compass']
+			},
+			options: {
+				livereload: true,
+			},
+			test: {
+				files: ['tests/spec/**/*.js'],
+				tasks: ['test']
+			},
+		},
+		clean: {
+		    dist: ['public/.tmp', 'dist/*'],
+		    server: 'public/.tmp'
+		},
+		concurrent: {
+		},
+		connect: {
+		    options: {
+		        port: 8000,
+		        hostname: 'localhost',
+		        livereload: true,
+		    },
+		    test: {
+		        options: {
+		            port: 8001,
+					target: 'http://localhost:8001/test.html',
+		        	open: true,
+		            middleware: function (connect) {
+		                return [
+		                    connect.static('tests'),
+		                    connect.static('public')
+		                ];
+		            }
+		        }
+		    },
+		    server: {
+		        options: {
+		        	open: true,
+		            middleware: function(connect) {
+		                return [
+		                    connect.static('public')
+		                ];
+		            }
+		        }
+		    },
+		},
+
+
+	});
+
+	grunt.registerTask('server', [
+		'clean:server',
+		'compass',
+		'connect:server',
+		'watch'
+	]);
+
+	grunt.registerTask('test', [
+		'connect:test',
+		'mocha',
+		'watch:test'
+	]);
+
+	grunt.registerTask('build', [
+	]);
+
+	grunt.registerTask('default', [
+		'jshint',
+		'server'
+	]);
+
+};
---
+++ public/scripts/app.js
@@ -0,0 +1,46 @@
+define([
+	'marionette',
+	'config/settings'
+], function(Marionette){
+
+	var App = new Marionette.Application();
+
+	App.addRegions({
+		headerRegion: '#header-region',
+		navRegion: '#nav-region',
+		mainRegion:'#main-region'
+	});
+
+	App.on('before:start', function(){
+
+	});
+
+	App.addInitializer(function(){
+		require(['modules/header/header_app','modules/nav/nav_app']);
+	});
+
+	App.on('start', function() {
+		require([
+
+		], function(){
+
+			if (Backbone.history) {
+				Backbone.history.start();
+			}
+
+			$(document).on('click', 'a:not([data-bypass])', function(e) {
+				var href = $(this).attr('href');
+				var protocol = this.protocol + '//';
+
+				if (href.slice(protocol.length) !== protocol) {
+					e.preventDefault();
+					Backbone.history.navigate(href, true);
+				}
+			});
+
+		});
+
+	});	
+
+	return App;
+});
