c20a5db0a56b6649e5016fbef5ce145a9ef68755
Kristoffer Robin Canlas <robin.canlas@geekgig.com>
2014-10-15 12:19:32 +0800

    change gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,107 +2,59 @@
 
 module.exports = function(grunt){
 
-	require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig({
-		compass: {
-			compile: {
-				options: {
-					config: 'config.rb',
-					force: true,
-				},
-			}
-		},
-		jshint: {
-		    options: {
-		        jshintrc: '.jshintrc',
-		        reporter: require('jshint-stylish'),
-		        ignores: ['public/scripts/libs/*']
-		    },
-		    all: [
-		        'Gruntfile.js',
-		        'public/scripts/{,*/}*.js',
-		    ]
-		},
-		mocha: {
-            urls: ['http://localhost:8001/test.html'],
-			options: {
-				run: true,
-				log: true,
-			}
-		},
-		watch: {
-			css: {
-				files: ['public/styles/**/*.scss'],
-				tasks: ['compass']
-			},
-			options: {
-				livereload: true,
-			},
-			test: {
-				files: ['tests/spec/**/*.js'],
-				tasks: ['test']
-			},
-		},
-		clean: {
-		    dist: ['public/.tmp', 'dist/*'],
-		    server: 'public/.tmp'
-		},
-		concurrent: {
-		},
-		connect: {
-		    options: {
-		        port: 8000,
-		        hostname: 'localhost',
-		        livereload: true,
-		    },
-		    test: {
-		        options: {
-		            port: 8001,
-					target: 'http://localhost:8001/test.html',
-		        	open: true,
-		            middleware: function (connect) {
-		                return [
-		                    connect.static('tests'),
-		                    connect.static('public')
-		                ];
-		            }
-		        }
-		    },
-		    server: {
-		        options: {
-		        	open: true,
-		            middleware: function(connect) {
-		                return [
-		                    connect.static('public')
-		                ];
-		            }
-		        }
-		    },
-		},
+  grunt.initConfig({
+    compass: {
+      compile: {
+        options: {
+          config: 'config.rb',
+          force: true,
+        },
+      }
+    },
+    watch: {
+      css: {
+        files: ['public/styles/**/*.scss'],
+        tasks: ['compass']
+      },
+    },
+    clean: {
+        dist: ['public/.tmp', 'dist/*'],
+        server: 'public/.tmp'
+    },
+    connect: {
+        options: {
+            port: 8000,
+            hostname: 'localhost',
+            // livereload: true,
+        },
+        server: {
+            options: {
+              open: true,
+                middleware: function(connect) {
+                    return [
+                        connect.static('public')
+                    ];
+                }
+            }
+        },
+    },
 
 
-	});
+  });
 
-	grunt.registerTask('server', [
-		'clean:server',
-		'compass',
-		'connect:server',
-		'watch'
-	]);
+  grunt.registerTask('server', [
+    'clean:server',
+    'compass',
+    'watch'
+  ]);
 
-	grunt.registerTask('test', [
-		'connect:test',
-		'mocha',
-		'watch:test'
-	]);
+  grunt.registerTask('build', [
+  ]);
 
-	grunt.registerTask('build', [
-	]);
-
-	grunt.registerTask('default', [
-		'jshint',
-		'server'
-	]);
+  grunt.registerTask('default', [
+    'server'
+  ]);
 
 };
